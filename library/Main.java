ffacf610ab8939c49429f2f94d6b9911fb94a520
ryoga404 <173412789+ryoga404@users.noreply.github.com>
2025-07-07 14:51:47 +0900

    test message

--- library/Main.java
+++ library/Main.java
@@ -3,6 +3,9 @@ package jp.ac.kcs.swing.library;
 public class Main {
     public static void main(String[] args) {
         //新しいメインフレームを作成
+    	System.out.println("Test Message")
     	new MainFrame();
     }
 }
+
+//TestEdit
