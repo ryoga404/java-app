31a362409032d7dde2828303deb22451e90ae3d6
ryoga <tech.ryoga@gmail.com>
2025-07-08 15:24:24 +0900

    Edit SessionDAO

--- kakeibo/SessionDAO.java
+++ kakeibo/SessionDAO.java
@@ -4,6 +4,7 @@ public class SessionDAO {
 
     //セッションを保存
     public boolean createSession(String sessionId, String userId) {
+    	//セッションIDをテーブルに追加
         String sql = "INSERT INTO Session(session_id, user_id) VALUES(?, ?)";
         try (Connection conn = DBUtil.getConnection();
             PreparedStatement pstmt =conn.prepareStatement(sql)) {
