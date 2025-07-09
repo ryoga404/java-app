f0b6273aa7f1c4b2019f4fdf0432dc8ed01f5a7b
mid <mid@KCSK3241.k302.kcsk.ac.jp>
2025-07-09 15:57:21 +0900

    te

--- kakeibo/Main.java
+++ kakeibo/Main.java
@@ -1,9 +1,8 @@
 //package kakeibo;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
     	System.out.println("Hello World");
+       	System.out.println("Hello World");
     }
 }
