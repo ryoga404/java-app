3d15d4de332029450721730628bec723317ed098
mid <mid@k302.kcsk.ac.jp>
2025-07-10 10:37:03 +0900

    test

--- kakeibo/LoginPanel.java
+++ kakeibo/LoginPanel.java
@@ -1,4 +1,9 @@
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 //ユーザ名とパスワードを入力するフォームとボタン
 
@@ -8,6 +13,35 @@ public class LoginPanel extends JPanel {
     public LoginPanel(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
 
+    }
+    
+    public void Login(MainFrame frame) {
+    	setLayout(new GridLayout(3, 2));
+    	//IDのテキストフィールド
+    	JTextField idField = new JTextField();
+    	//パスワードのテキストフィールド
+    	JTextField passField = new JTextField();
+    	
+        //登録ボタン
+    	JButton  loginBtn = new JButton("ログイン");
+        //TOPに戻るボタン
+    	JButton  backBtn = new JButton("TOPへ戻る");//TOPに戻るボタン
+    	
+    	//ログインボタンが押されたら
+    	loginBtn.addActionListener(e -> {
+    		
+        });
+    	
+    	//TOPに戻るボタン
+    	backBtn.addActionListener(e -> frame.showPanel("TOP"));
+    	
+    	//カードに部品を追加
+    	add(new JLabel("ユーザーID"));
+        add(idField);
+        add(new JLabel("パスワード"));
+        add(passField);
+        add(loginBtn);
+        add(backBtn);
     }
 
 }
